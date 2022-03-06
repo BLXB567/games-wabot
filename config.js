75389ba9f0f01eeb30c4d59de773191960a2e2d2
BLZXB5 <97624291+BLXB567@users.noreply.github.com>
2022-03-06 18:47:25 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 global.DeveloperMode = 'true' //true Or false
-global.linkGC = ['https://chat.whatsapp.com/Lb4Emjih98rBiCZiZoS2eM', 'https://chat.whatsapp.com/FrluVyY5kBRIhY6yKfH4DI']
+global.linkGC = ['https://chat.whatsapp.com/DebwMOR22xV9dzToTx4NU8', 'https://chat.whatsapp.com/Ks7jmQhVdKgATdYwcsAhlL']
 global.Owner = {
   // use the phone number with the country code, for example Indonesia '62' America '1' etc
   '6283117436733': { // put your number here
